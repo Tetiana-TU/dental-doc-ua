9834a260c1e9e4be54150ea7595b57bf07c97500
Tetiana Dmytryienko <dmitrienko_tanya@ukr.net>
2026-06-16 14:05:13 +0300

    servercorrect

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use("/api/doctors", doctorsRoutes);
 app.use("/api/form037", form037Routes);
 app.use("/api/form039", form039Routes);
 app.use(express.static(path.join(__dirname, "../dist")));
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../dist/index.html"));
 });
 app.listen(PORT, () => {
