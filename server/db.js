b2375072ee1c1c5f62ed65993c40901ac8e094fd
Tetiana Dmytryienko <dmitrienko_tanya@ukr.net>
2026-06-23 14:06:59 +0300

    postgre

--- server/db.js
+++ server/db.js
@@ -5,12 +5,23 @@ dotenv.config();
 
 const { Pool } = pg;
 
-const pool = new Pool({
-  user: process.env.DATABASE_USER,
-  host: process.env.DATABASE_HOST,
-  database: process.env.DATABASE_NAME,
-  password: process.env.DATABASE_PASSWORD,
-  port: process.env.DATABASE_PORT,
-});
+const isProduction = process.env.DATABASE_URL;
+
+const pool = new Pool(
+  isProduction
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false, // Обов'язково для Render
+        },
+      }
+    : {
+        user: process.env.DATABASE_USER,
+        host: process.env.DATABASE_HOST,
+        database: process.env.DATABASE_NAME,
+        password: process.env.DATABASE_PASSWORD,
+        port: process.env.DATABASE_PORT,
+      },
+);
 
 export default pool;
